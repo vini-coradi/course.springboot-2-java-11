257d5ab43dd2ffd27dc8cf5f7520cea95191fb2f
vini-coradi <vinicesar.coradi@gmail.com>
2020-05-24 16:49:00 -0300

    H2 database, test profile, JPA

--- src/main/java/com/educandoweb/course/entities/User.java
+++ src/main/java/com/educandoweb/course/entities/User.java
@@ -2,10 +2,17 @@ package com.educandoweb.course.entities;
 
 import java.io.Serializable;
 
-public class User implements Serializable {
-	
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
+@Entity
+public class User implements Serializable {
 	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String name;
 	private String email;
@@ -13,7 +20,6 @@ public class User implements Serializable {
 	private String password;
 	
 	public User() {
-		
 	}
 
 	public User(Long id, String name, String email, String phone, String password) {
@@ -89,6 +95,4 @@ public class User implements Serializable {
 			return false;
 		return true;
 	}
-	
-
 }
